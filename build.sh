c399cf5cc2f4f75581d7fde26acd3f8dab0620db
Matt Mohandiss <mattmohandiss@gmail.com>
2023-12-10 16:38:46 -0500

    Remove output directory

--- build.sh
+++
@@ -1,8 +0,0 @@
-# COPY STYLE
-cp style.css out/style.css
-
-# BUILD HTML
-pandoc resume.md -c style.css -s --template template.html -o out/resume.html
-
-# BUILD PDF
-pandoc resume.md -c style.css -s --template template.html -t html -o out/resume.pdf
